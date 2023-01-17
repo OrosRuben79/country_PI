c7c90f97511d19480d4799c733a103c5e1ccc89b
OrosRuben79 <109885291+OrosRuben79@users.noreply.github.com>
2023-01-17 13:02:05 -0300

    agrego all cors

--- backend/api/src/app.js
+++ backend/api/src/app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
+const cors = requiere('cors')
+
 require('./db.js');
 
 const server = express();
@@ -14,19 +16,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-
-  const allowedOrigins = [ 'http://localhost:3000' || 'https://country-client-pi-gwui.vercel.app']; // update to match the domain you will make the request from
-	const origin = req.headers.origin;
-	if (allowedOrigins.includes(origin)) {
-		res.setHeader('Access-Control-Allow-Origin', origin);
-	}
-  // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+server.use(cors()); 
 
 server.use('/', routes);
 
