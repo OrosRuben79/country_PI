265221a5ced598eb4e179b13bffef1d32041c02b
Rubén Oros <orosruben79@gmail.com>
2025-08-14 11:58:43 -0300

    modifico el archivo index

--- backend/index.js
+++ backend/index.js
@@ -19,13 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = process.env.PORT || 3001; 
 
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+const PORT = process.env.PORT || 3001;
+
+// Sync models sin borrar datos
+conn.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`Server listening at ${PORT}`);
   });
 });
-
-
